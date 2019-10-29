ab6db54b89cb64d9ccf9d255e7fbf7f3d0682022
jimenalozano <jilozano@itba.edu.ar>
2019-10-29 18:32:17 -0300

    client and server using hazelcast

---
+++ server/src/main/java/g11tpe/server/Movement.java
@@ -0,0 +1,21 @@
+package g11tpe.server;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
+
+import java.io.IOException;
+
+public class Movement implements DataSerializable {
+
+
+    @Override
+    public void writeData(ObjectDataOutput objectDataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readData(ObjectDataInput objectDataInput) throws IOException {
+
+    }
+}
