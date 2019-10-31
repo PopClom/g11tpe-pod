95a8fe073973c4a9ed9385bdcbdd30227dadcb1e
Maite <mherran@itba.edu.ar>
2019-10-31 18:40:58 -0300

    configurations

--- client/src/main/java/g11tpe/client/Client.java
+++ client/src/main/java/g11tpe/client/Client.java
@@ -1,11 +1,20 @@
 package g11tpe.client;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
 import com.hazelcast.config.Config;
+import com.hazelcast.config.GroupConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
+import com.hazelcast.core.IMap;
+import g11tpe.Movement;
 import g11tpe.client.exceptions.InvalidProgramParametersException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.Paths;
 
 public class Client {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
@@ -22,6 +31,30 @@ public class Client {
             logger.error(e.getMessage());
             System.exit(-1);
         }
+
+
+        ClientConfig config = new ClientConfig();
+        GroupConfig groupConfig = config.getGroupConfig();
+        groupConfig.setName("grupo-11").setPassword("chau");
+
+        ClientNetworkConfig networkConfig = config.getNetworkConfig();
+        for (String address : parameters.getAddresses()) {
+            networkConfig.addAddress(address);
+        }
+
+        HazelcastInstance hzClient = HazelcastClient.newHazelcastClient(config);
+
+        //probando que funcione
+        IMap<Long, String> map = hzClient.getMap("data");
+        System.out.println(map.values());
+
+        IMap<String, Airport> airportIMap;
+        IList<Movement> movementsIList;
+
+
+
+
+
 
 
     }
