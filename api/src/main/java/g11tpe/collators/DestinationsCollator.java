718782af1c02ba029a1a07d1d1fad2a6a0d499de
jimenalozano <jilozano@itba.edu.ar>
2019-10-31 17:36:56 -0300

    order

--- api/src/main/java/g11tpe/collators/DestinationsCollator.java
+++ api/src/main/java/g11tpe/collators/DestinationsCollator.java
@@ -3,8 +3,10 @@ package g11tpe.collators;
 import com.hazelcast.mapreduce.Collator;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 public class DestinationsCollator implements Collator<Map.Entry<String, Long>, Map<String, Long>> {
 
@@ -16,9 +18,8 @@ public class DestinationsCollator implements Collator<Map.Entry<String, Long>, M
             map.put(stringLongEntry.getKey(), stringLongEntry.getValue());
         });
 
-/*
-        return map.entrySet().stream().sorted(Map.Entry.comparingByValue());
-*/
+        return map.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(
+                Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
     }
 }
